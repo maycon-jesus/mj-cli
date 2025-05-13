6c66c54bc2ccc08b6496dd386698a12de7f9c3ec
Maycon Jesus <contato@mayconjesus.dev>
2025-05-13 00:24:28 -0300

    fix: check file exists windows

--- utils/myIo/asserts.go
+++ utils/myIo/asserts.go
@@ -1,24 +1,21 @@
 package myIo
 
 import (
-	"github.com/spf13/cobra"
 	"os"
-	"strings"
 )
 
 func DirectoryExists(path string) bool {
 	info, err := os.Stat(path)
-	cobra.CheckErr(err)
+	if err != nil {
+		return false
+	}
 	return info.IsDir()
 }
 
 func FileExists(path string) bool {
 	info, err := os.Stat(path)
 	if err != nil {
-		if strings.HasSuffix(err.Error(), "no such file or directory") {
-			return false
-		}
-		cobra.CheckErr(err)
+		return false
 	}
 	return !info.IsDir()
 }
